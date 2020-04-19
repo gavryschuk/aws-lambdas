f4bb0874ebae7f5678d5475824012040cb177f7a
Anatolii Havryshchuk <a.v.gavryschuk@gmail.com>
2020-04-19 18:45:16 +0300

    move analytics handler into a separate folder

---
+++ src/analytics/index.ts
@@ -0,0 +1,42 @@
+import { Handler, Context, Callback } from 'aws-lambda';
+import { DynamoDB, AWSError } from 'aws-sdk';
+
+const DynamoDBClient = new DynamoDB.DocumentClient();
+const DBTable = "prod-eventLogs";
+
+/**
+ * puts the data into Dynamo DB
+ * @param eventLog  - stringified JSON data object
+ */
+const putDataIntoDynamoDB = async(eventLog:any) => {
+  return await DynamoDBClient.put({
+    TableName: DBTable,
+    Item: eventLog
+  }).promise()
+}
+
+/**
+ * Lambda Function Handler
+ * @param event - input data. Required Params: eventLog:string
+ * @param context - context
+ * @param callback - function callback
+ */
+const handler: Handler = (event: any, context: Context, callback: Callback) => {
+  
+  // check if income params are provided
+  if(!event.eventLog) { callback(new Error("Please provide a value for eventLog parameter")); return; }
+  try{ 
+    // parse income eventLog into JSON
+    const eventLog = JSON.parse(event.eventLog); 
+
+    putDataIntoDynamoDB(eventLog).then((data:DynamoDB.DocumentClient.PutItemOutput) => { 
+      callback(undefined,data); 
+    }).catch((err:AWSError) => {
+      callback(err);
+    })
+
+  }catch(e){ callback(new Error("eventLog parameter has to be a stringified JSON object: "+e.toString())); return; }
+
+};
+
+export { handler }
